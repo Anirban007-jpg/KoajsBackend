5e75984dbc82de4a45698ae61b9a85890d137dd6
Anirban Banerjee <abanerjee763@gmail.com>
2025-10-08 20:40:41 +0530

    ..

--- controllers/creditor.js
+++ controllers/creditor.js
@@ -7,15 +7,25 @@ exports.createCreditor = async (ctx) => {
         const token = ctx.request.token;
         const authUserId = await jwtDecode(token);
         const individual = authUserId._id;
-        const presentcreditor = await Creditor.findOne({ Name: ctx.request.body.Debtor_name });
-        if (!presentcreditor) {
-            const newcreditor = new Creditor({ Creditor_name,Creditor_adrress,Creditor_contact_no,Creditor_email,Creditor_Balance,individual });
-            await newcreditor.save();
-            ctx.status = 200;
-            ctx.body = { message: `${Creditor_name} Created Successfully`, debtor: newcreditor };
+        const ledger = await Ledger.findOne({ Ledger_Name: "Creditor A/C" }); 
+        // console.log(ledger)
+        if (ledger) {
+            const presentcreditor = await Creditor.findOne({ Creditor_name: Creditor_name });
+            // console.log(presentdebtor)
+            if (!presentcreditor) {
+                const newcreditor = new Debtor({ Creditor_name,Creditor_adrress,Creditor_contact_no,Creditor_email,Creditor_Balance,Balance : {Previous_Balance: Creditor_Balance, Previous_Balance_Date: Date.now()}, individual, ledger: ledger._id,Creditor_Balance_Type : ledger.balance_type });
+                await newcreditor.save();
+                await Ledger.findOneAndUpdate({Ledger_Name : "Creditor A/C"}, {$push : {Creditor: newcreditor._id}}, {upsert: true});
+                ctx.status = 200;
+                ctx.body = { message: `${Creditor_name} Created Successfully... Update Ledger Balance immediately`, creditor: newcreditor };
+            }
+            else {
+                ctx.body = { message: `${Creditor_name} Already exsists` };
+            }
         }
         else {
-            console.log("Creditor alreay exists");
+            ctx.body = { message: `Create Creditor Ledger A/C First`};
+           
         }
     } catch (error) {
         console.log(error);
@@ -42,22 +52,41 @@ exports.getCreditors = async (ctx) => {
     }
 }
 
-// exports.updateCreditor= async (ctx) => {
-//     const {DebtorName, updateValue} = ctx.request.body;
-//     try{
-//         let debtors = await Ledger.findOne({Debtor_name: DebtorName});
-//         await debtors.updateOne({name : debtors.Debtor_name}, {Current_Balance: updateValue});
-//         ctx.status = 200;
-//         ctx.body = {  message: `${debtors.Debtor_name} Updated`, debtors };
-//     } catch (error) {
-//         console.log(error);
-//         if (error.code === 11000) {
-//             // Handle duplicate key errors (e.g., unique fields)
-//             ctx.status = 409; // Conflict
-//             ctx.body = { message: 'Duplicate entry', error: 'Ledger Name already exists' };
-//         }
-//     }
-// }
+exports.updateCreditor = async (ctx) => {
+    try{
+        let ledger = await Ledger.findOne({ Ledger_Name: "Creditor A/C" });
+        // console.log(ledger);
+
+        // console.log(ledger.Current_Balance);
+        // console.log(ledger.Debtors[0]._id);
+
+        let sum = 0;
+        for (i=0; i<ledger.Creditors.length; i++){
+            const result= await Creditor.findById({_id: ledger.Creditors[i]._id});
+            // console.log(result);
+            sum = math.add(sum,result.Creditor_Balance);
+            // console.log(sum);
+        }
+        // console.log(sum);
+        let newSum = math.add(sum,ledger.Opening_Balance);
+        if (math.add(ledger.Opening_Balance,sum) == ledger.Current_Balance){
+            ctx.status = 200;
+            ctx.body = { message: `Creditor Ledger already Updated`};
+        }
+        else{
+            await Ledger.findOneAndUpdate({Ledger_Name: ledger.Ledger_Name}, {Current_Balance: newSum, $push : {Balance: {Previous_Balance: sum, Previous_Balance_Date: Date.now()}}});
+            ctx.status = 200;
+            ctx.body = { message: `Creditor Ledger Updated`};
+        }
+    } catch (error) {
+        console.log(error);
+        if (error.code === 11000) {
+            // Handle duplicate key errors (e.g., unique fields)
+            ctx.status = 409; // Conflict
+            ctx.body = { message: 'Duplicate entry', error: 'Ledger Name already exists' };
+        }
+    }
+}
 
 // exports.deleteLedgers = async (ctx) => {
 //     const {LedgerName} = ctx.request.body;
--- routes/creditorRoutes.js
+++ routes/creditorRoutes.js
@@ -1,7 +1,7 @@
 const Router = require('koa-router');
 const { requireSignin, CompanyMiddleware } = require('../controllers/authController');
 const validateCreditor = require('../validators/creditor');
-const { createCreditor, getCreditors } = require('../controllers/creditor');
+const { createCreditor, getCreditors, updateCreditor } = require('../controllers/creditor');
 
 
 
@@ -9,6 +9,6 @@ const { createCreditor, getCreditors } = require('../controllers/creditor');
 const router = new Router();
 router.post('/creditor/create', requireSignin, CompanyMiddleware, validateCreditor, createCreditor);
 router.get('/creditor/get', requireSignin, getCreditors);
-
+router.get('/creditor/update', requireSignin, updateCreditor);
 
 module.exports = router;
